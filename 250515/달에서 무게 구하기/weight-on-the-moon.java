123e350301c16c5b3d0f852aaf17cfc9e1fbc5fa
__Alpha <129605955+ssedpspjm@users.noreply.github.com>
2025-05-15 10:17:20 +0900

    Title: 달에서 무게 구하기, Time: 108ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250515/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int wei = 13;
+        double grav = 0.165000;
+
+        //System.out.printf(wei + " * " + grav + " = " + (wei*grav));
+        System.out.printf("%d * %.6f = %.6f",wei, grav, wei*grav);
+    }
+}
