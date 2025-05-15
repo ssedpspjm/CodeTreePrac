650c6f69fa4bba25f2748b6da602fc0eae06a4e9
__Alpha <129605955+ssedpspjm@users.noreply.github.com>
2025-05-15 09:44:19 +0900

    Title: 2줄 출력, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250515/2줄 출력/print-two-lines.py
+++ 250515/2줄 출력/print-two-lines.py
@@ -1,2 +1,6 @@
-print ('Hello')
-print ('World')
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
