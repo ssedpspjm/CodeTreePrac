bda9ded6fca8e39f30d4c04b3fc334eb482e8cd8
__Alpha <129605955+ssedpspjm@users.noreply.github.com>
2025-05-15 11:43:29 +0900

    Title: 점수 입출력, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250515/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+
+        N = sc.nextInt();
+        
+        System.out.printf("Your score is " + N + " point.");
+    }
+}
