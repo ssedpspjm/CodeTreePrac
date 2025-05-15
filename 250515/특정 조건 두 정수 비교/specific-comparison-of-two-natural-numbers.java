053c738c861309e21c2353c3b3691ef10cb17c5a
__Alpha <129605955+ssedpspjm@users.noreply.github.com>
2025-05-15 12:58:45 +0900

    Title: 특정 조건 두 정수 비교, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250515/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a < b) {
+            System.out.printf(1 + " ");
+
+            if (a == b) {
+                System.out.print(1);
+            }
+            else {
+                System.out.print(0);
+            }
+        }
+        else {
+            System.out.printf(0 + " ");
+
+            if (a == b) {
+                System.out.print(1);
+            }
+            else {
+                System.out.print(0);
+            }
+        }
+    }
+}
